7667c66cfde02387c9a004aa109cb134d815d162
unknown <srkanilsharma@gmail.com>
2019-08-02 00:06:10 +0530

    [Add] inde.js file

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+
+const userController = require('./routers/userController');
+const commentController = require('./routers/commentController');
+
+const app = express();
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// CORS Middleware
+app.use(cors({ origin: '*' }));
+
+//Port Number
+const port = process.env.PORT || 8080;
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',(req,res)=>{
+    return res.send('index.js')
+})
+app.use('/users', userController);
+app.use('/comments', commentController);
+
+// Start Server
+app.listen(port, () => console.log('Server started at port :',port));
