709f9f3ddca2ffd4d260b33772d14635e8d310fc
unknown <srkanilsharma@gmail.com>
2019-08-25 18:01:10 +0530

    [Add] login and register component

--- angular/src/app/components/login/login.component.ts
+++ angular/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/shared/auth.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  LoginForm:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private authService : AuthService,
+    private router : Router,
+    private toastr : ToastrService) { }
 
   ngOnInit() {
+    this.LoginForm = this.formBuilder.group({
+      username:[null, Validators.required],
+      password: [null, Validators.required]
+    })
   }
 
+  onLogInClick(){
+    if(this.LoginForm.invalid){
+      return;
+    }
+    const loginData = {
+      username : this.LoginForm.value.username,
+      password : this.LoginForm.value.password
+    }
+    this.authService.authenticateUser(loginData).subscribe(response =>{
+      if(response['success']){
+        this.authService.storeUserData(response['token'], response['user']);
+        this.toastr.success('Successfully Logged In');
+        this.router.navigate(['/']);
+      }
+      else{
+        this.toastr.error(response['msg']);
+        this.router.navigate(['/login']);
+      }
+    },
+    (err) =>{
+      this.toastr.error('Username or password is incorrect');
+    })
+  }
 }
--- angular/src/app/components/register/register.component.ts
+++ angular/src/app/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/shared/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  registerForm:FormGroup;
+  passwordRegExp = environment.passwordPattern;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+      private authService : AuthService,
+      private toastr : ToastrService,
+      private router : Router) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      name:[null,Validators.required],
+      username:[null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.pattern(this.passwordRegExp)]]
+    })
+  }
+  
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  onSignupClick(){
+    // if(this.registerForm.invalid){
+    //   return;
+    // }
+    const signupData = {
+      name : this.registerForm.value.name,
+      username : this.registerForm.value.username,
+      email : this.registerForm.value.email,
+      password : this.registerForm.value.password
+    }
+
+    this.authService.registerUser(signupData).subscribe(response =>{
+        if(response['success']){
+          this.toastr.success('You are now registered and can now login');
+          this.router.navigate(['/login']);
+        }
+        else{
+          this.toastr.error('Something went wrong');
+          this.router.navigate(['/register']);
+        }
+    })
   }
 
 }
