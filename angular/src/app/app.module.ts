3d479ab48ab85c915f2c219ecae86a998c31af4d
unknown <srkanilsharma@gmail.com>
2019-08-25 18:03:14 +0530

    [Add] home component and update app modules

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,26 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { AuthService} from '../app/shared/auth.service';
+import { HttpClientModule } from '@angular/common/http'; 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/src/app/components/home/home.component.ts
+++ angular/src/app/components/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../shared/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,115 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  commentDescription: FormGroup;
+  loadedComments: any;
+
+  constructor(
+    private authService : AuthService,
+    private formBuilder: FormBuilder,
+    private router : Router,
+    private toastr : ToastrService
+  ) { }
 
   ngOnInit() {
+    this.authService.getCommentList().subscribe( res => {
+      console.log("loaded comments are ",res);
+      this.loadedComments = res;
+    });
+
+    this.commentDescription = this.formBuilder.group({
+      comment: [null ,Validators.required]
+    });
   }
 
+  onComment(){
+    if(this.commentDescription.value.comment == null){
+      this.toastr.error('Comment field is required');
+      return;
+    }
+    if(!this.authService.loggedIn()){
+      this.toastr.error('Please Login');
+      this.router.navigate(['/login']);
+      return;
+    }
+    let comment  = this.commentDescription.value.comment;
+
+    this.authService.comment(comment).subscribe(data => {
+      console.log("comment posted ", data);
+      this.loadedComments.push(data);
+      this.toastr.success('Comment posted successfully');
+    },
+    (err) =>{
+      console.log(err);
+    })
+  }
+
+  onUpvote(comment){
+    if(!this.authService.loggedIn()){
+      this.toastr.error('Please Login');
+      this.router.navigate(['/login']);
+      return;
+    }
+    let user = this.authService.loadUser();
+    let userId = user.id;
+    if(comment.author.id == userId){
+      this.toastr.error('You cant upvote your own comment');
+      return;
+    }
+
+    if(comment.downvote.includes(userId)){
+      this.toastr.error('You have already downvoted');
+      return;
+    }
+
+    if(comment.upvote.includes(userId)){
+      this.toastr.error('You have already upvoted');
+      return;
+    }
+    const upvoteData = {
+      comment_id : comment._id,
+      id : user.id
+    }
+    this.authService.commentupvote(upvoteData).subscribe(data => {
+      this.toastr.success('Comment upvoted successfully');
+      comment.upvote.push(upvoteData.id);
+      this.commentDescription.value.comment = '';
+            //this.router.navigate(['/']);
+    },
+    (err)=>{
+      console.log(err);
+    })
+  }
+
+  onDownvote(comment){
+    if(!this.authService.loggedIn()){
+      this.toastr.error('Please Login');
+      this.router.navigate(['/login']);
+      return;
+    }
+    let user = this.authService.loadUser();
+    let userId = user.id;
+    if(comment.author.id == userId){
+      this.toastr.error('You can not downvote your own comment');
+      return;
+    }
+
+    if(comment.downvote.includes(userId)){
+      this.toastr.error('You have already downvoted');
+      return;
+    }
+
+    if(comment.upvote.includes(userId)){
+      this.toastr.error('You have already upvoted');
+      return;
+    }
+    const upvoteData = {
+      comment_id : comment._id,
+      id : user.id
+    }
+    this.authService.commentdownvote(upvoteData).subscribe(data => {
+      this.toastr.success('Comment downvoted successfully');
+      comment.downvote.push(upvoteData.id);
+    })
+  }
 }
